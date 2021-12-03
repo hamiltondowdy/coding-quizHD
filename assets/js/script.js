feaf0582e44082613362bb3153c126b03c784b5c
hamiltondowdy <hddowdy@ncsu.edu>
2021-12-03 10:45:37 -0600

    added function to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
-var questions [ 
+var questions = [
     {
-        title: "1text text?",
-        choices: ["text", "text", "text", "text"],
-        answer: "Text"
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-        title: "2text text?",
-        choices: ["text", "text", "text", "text"],
-        answer: "Text"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
     {
-        title: "3text text?",
-        choices: ["text", "text", "text", "text"],
-        answer: "Text"
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
     },
     {
-        title: "4text text?",
-        choices: ["text", "text", "text", "text"],
-        answer: "Text"
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
     },
     {
-        title: "5text text?",
-        choices: ["text", "text", "text", "text"],
-        answer: "Text"
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "Terminal / Bash", "for loops", "console log"],
+        answer: "console log"
     },
 ];
 
@@ -33,3 +33,35 @@ var currTime = document.querySelector("#currtime");
 var timer = document.querySelector("#timer");
 var quizQuestions = document.querySelector("#quizQuestions");
 var wrapper = document.querySelector("#wrapper");
+
+
+// Seconds left from start (15 per question + 2)
+var secondsLeft = 77;
+
+// Interval time
+var holdInterval = 0;
+
+// Hold penalty time
+var holdPenalty = 10;
+
+// Creat Element - unordered list
+var ulCreate = document.createElement("ul");
+
+// Adding functionality to button
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function() {
+        secondsLeft--;
+        currTime.textContent = "Time: " + secondsLeft;
+        
+        if (secondsLeft <= 0) {
+            clearIntercal(holdInterval);
+            allDone();
+            currentTime.textContent = "Time's Up!";
+        }
+    }, 1000);
+}
+    return(questionIndex);
+});
+
+
